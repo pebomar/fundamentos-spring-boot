74b7a8375d0b2f6fb4dedb1a9b535a8319901387
Omar <peb.omar@hotmail.com>
2022-02-19 20:59:38 -0600

    Uso de anotación value para apuntar a properties

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
@@ -20,6 +22,18 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name, apellido);
@@ -28,10 +42,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource generarDataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
